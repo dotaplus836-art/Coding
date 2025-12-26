12ec854a0ab789cdcc4a0cdec70572ac46b4d2f6
Romchik <dotaplus836@gmail.com>
2025-12-26 22:59:26 +0300

    New Program

--- FirstProgram/FirstProgram/Program.cs
+++ FirstProgram/FirstProgram/Program.cs
@@ -6,7 +6,23 @@ namespace FirstProgram
     {
         private static void Main(string[] args)
         {
+            Console.Write("Введите первое число: ");
+            int firstNumber = int.Parse(Console.ReadLine());
 
+            Console.Write("Введите второе число: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            CompareNumbers(firstNumber, secondNumber);
+        }
+
+        private static void CompareNumbers(int firstNumber, int secondNumber)
+        {
+            if (firstNumber > secondNumber)
+                Console.WriteLine($"{firstNumber} > {secondNumber}");
+            else if (firstNumber < secondNumber)
+                Console.WriteLine($"{firstNumber} < {secondNumber}");
+            else
+                Console.WriteLine($"{firstNumber} == {secondNumber}");
         }
     }
 }
